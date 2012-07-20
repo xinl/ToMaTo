cb70ddeddb7416754225bf1e7da2b418b0125e99
Xin Li <mr.xin.li@gmail.com>
2012-07-20 01:21:09 -0400

    Add unit test for PolicyTerm

---
+++ edu.upenn.cis.tomato/src/edu/upenn/cis/tomato/test/PolicyTermTest.java
@@ -0,0 +1,79 @@
+package edu.upenn.cis.tomato.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.upenn.cis.tomato.core.PolicyTerm;
+import edu.upenn.cis.tomato.core.PolicyTerm.ComparatorType;
+import edu.upenn.cis.tomato.core.PolicyTerm.PropertyName;
+
+public class PolicyTermTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testIsStatic() {
+		PolicyTerm pt1 = new PolicyTerm("SiteName", ComparatorType.EQUAL, "Blah.");
+		PolicyTerm pt2 = new PolicyTerm("TimeInvoked", ComparatorType.LESS_THAN, new Integer(3));
+		assertTrue(pt1.isStatic());
+		assertFalse(pt2.isStatic());
+	}
+
+	@Test
+	public void testNegate() {
+		PolicyTerm pt1 = new PolicyTerm("SiteName", ComparatorType.EQUAL, "Blah.");
+		PolicyTerm pt2 = new PolicyTerm("SiteName", ComparatorType.UNEQUAL, "Blah.");
+		PolicyTerm pt3 = new PolicyTerm("SiteName", ComparatorType.GREATER_THAN, "Blah.");
+		PolicyTerm pt4 = new PolicyTerm("SiteName", ComparatorType.LESS_THAN, "Blah.");
+		PolicyTerm pt5 = new PolicyTerm("SiteName", ComparatorType.GREATER_EQUAL_THAN, "Blah.");
+		PolicyTerm pt6 = new PolicyTerm("SiteName", ComparatorType.LESS_EQUAL_THAN, "Blah.");
+
+		assertEquals(pt2, pt1.negate());
+		assertEquals(pt1, pt2.negate());
+		assertEquals(pt6, pt3.negate());
+		assertEquals(pt5, pt4.negate());
+		assertEquals(pt4, pt5.negate());
+		assertEquals(pt3, pt6.negate());
+
+		assertNotSame(pt2, pt1.negate());
+	}
+
+	@Test
+	public void testEqualsObject() {
+		PolicyTerm pt1 = new PolicyTerm("SiteName", ComparatorType.EQUAL, "Blah.");
+		PolicyTerm pt2 = new PolicyTerm("SiteName", ComparatorType.EQUAL, "Blah.");
+		PolicyTerm pt3 = new PolicyTerm("CallerName", ComparatorType.EQUAL, "Blah.");
+		PolicyTerm pt4 = new PolicyTerm("SiteName", ComparatorType.GREATER_EQUAL_THAN, "Blah.");
+		PolicyTerm pt5 = new PolicyTerm("SiteName", ComparatorType.EQUAL, "Blue.");
+		PolicyTerm pt6 = new PolicyTerm(PropertyName.SITE_NAME, ComparatorType.EQUAL, new String("Blah."));
+
+		assertTrue(pt1.equals(pt2));
+		assertFalse(pt1.equals(pt3));
+		assertFalse(pt1.equals(pt4));
+		assertFalse(pt1.equals(pt5));
+		assertTrue(pt1.equals(pt6));
+	}
+
+	@Test
+	public void testAppliesTo() {
+		PolicyTerm pt1 = new PolicyTerm("SiteName", ComparatorType.EQUAL, "Blah.");
+		PolicyTerm pt2 = new PolicyTerm("SiteStartOffset", ComparatorType.LESS_EQUAL_THAN, new Integer(3));
+
+		assertTrue(pt1.appliesTo("Blah."));
+		assertTrue(pt1.appliesTo(new String("Blah.")));
+		assertFalse(pt1.appliesTo("Blue."));
+		assertFalse(pt1.appliesTo(new Integer(2)));
+
+		assertTrue(pt2.appliesTo(new Integer(3)));
+		assertTrue(pt2.appliesTo(new Integer(1)));
+		assertTrue(pt2.appliesTo(new Integer(-3)));
+		assertFalse(pt2.appliesTo(new Integer(4)));
+		assertFalse(pt2.appliesTo("3"));
+
+	}
+
+}
